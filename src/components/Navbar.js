c109c46593a8ea28f79866144cebfa60ce313edb
Pedro Garcia <drogpe@gmail.com>
2021-01-13 11:17:07 -0300

    Added skeleton navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import logo from './logo.png'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className='container flex'>
+            <img src={logo} alt='logo' />
+            <nav>
+                <ul className='flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    <li><button>Start Free Trial</button></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import 'tachyons'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
