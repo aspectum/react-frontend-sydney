a40b0211021b275096fa4ba75bc84dc9d5e7b56c
Pedro Garcia <drogpe@gmail.com>
2021-01-14 17:55:41 -0300

    Intro: refined styling

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import screencap from './screencap.png';
 import './Intro.css'
 
@@ -7,9 +8,9 @@ function Intro() {
             <div className='intro tc container'>
                 <h1>Smart Tool Manage Your Business</h1>
                 <p>Sydney brings plenty of customization possibilities like access to all Google Fonts, full color control, layout control, logo upload, full screen slider, header image, sticky navigation and much more.</p>
-                <div className='buttons'>
-                    <button>Start Free Trial</button>
-                    <button>See Pricing</button>
+                <div>
+                    <a href='#' className='button'>Start Free Trial</a>
+                    <a href='#' className='button btn-2'>See Pricing</a>
                 </div>
                 <div>
                     <img src={screencap} alt='screencap' width='100%'></img>
