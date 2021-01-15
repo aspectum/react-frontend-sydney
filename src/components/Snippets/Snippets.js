69379aae859ce74c8b1a5418dde361a4a2ce0cb4
Pedro Garcia <drogpe@gmail.com>
2021-01-15 11:21:25 -0300

    Snippets: refined styling

--- src/components/Snippets/Snippets.js
+++ src/components/Snippets/Snippets.js
@@ -1,21 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import snippets from './snippets.png';
+import './Snippets.css'
 
 function Snippets() {
     return (
         <div className='section-wrapper'>
-            <div className='flex tc'>
-                <div>
+            <div className='snippets container'>
+                <div className='screenshot'>
                     <img src={snippets} alt=''></img>
                 </div>
-                <div>
+                <div className='description'>
                     <h2>Simple, Intuitive And Easy To Use</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                     <ul>
-                        <li>The text should be more aligned and in a small</li>
-                        <li>Contrary to popular belief, Lorem Ipsum is not simply</li>
-                        <li>All the Lorem Ipsum generators on the Internet tend</li>
-                        <li>Sed ut perspiciatis unde omnis iste natus error sit</li>
-                        <li>All the Lorem Ipsum generators on the Internet tend</li>
+                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>The text should be more aligned and in a small</span></li>
+                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>Contrary to popular belief, Lorem Ipsum is not simply</span></li>
+                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>All the Lorem Ipsum generators on the Internet tend</span></li>
+                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>Sed ut perspiciatis unde omnis iste natus error sit</span></li>
+                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>All the Lorem Ipsum generators on the Internet tend</span></li>
                     </ul>
                 </div>
             </div>
