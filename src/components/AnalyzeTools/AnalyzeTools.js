e10c9eab24e10a1700e452fc831c12a4af109b3f
Pedro Garcia <drogpe@gmail.com>
2021-01-15 16:38:57 -0300

    AnalyzeTools: cleaned up the code

--- src/components/AnalyzeTools/AnalyzeTools.js
+++ src/components/AnalyzeTools/AnalyzeTools.js
@@ -5,7 +5,18 @@ import './AnalyzeTools.css'
 import { faPlus, faWindowMinimize } from '@fortawesome/free-solid-svg-icons'
 
 // https://www.npmjs.com/package/react-collapsible
-// Quite messy, maybe should make new component
+
+function collapsible_header(header_text) {
+    return (
+        <div>
+            <span className='wrapper'>
+                <span>{header_text}</span>
+                <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
+                <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
+            </span>
+        </div>
+    )
+}
 
 function AnalyzeTools() {
     return (
@@ -19,51 +30,19 @@ function AnalyzeTools() {
                         <h2>Analyze Your Data With Our Analyze Tools</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                         <div>
-                            <Collapsible trigger={
-                                <div>
-                                    <span className='wrapper'>
-                                        <span>Organize your data</span>
-                                        <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
-                                        <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
-                                    </span>
-                                </div>
-                            }>
+                            <Collapsible trigger={collapsible_header('Organize your data')}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
                             </Collapsible>
                             <hr></hr>
-                            <Collapsible trigger={
-                                <div>
-                                    <span className='wrapper'>
-                                        <span>Always in sync</span>
-                                        <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
-                                        <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
-                                    </span>
-                                </div>
-                            }>
+                            <Collapsible trigger={collapsible_header('Always in sync')}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
                             </Collapsible>
                             <hr></hr>
-                            <Collapsible trigger={
-                                <div>
-                                    <span className='wrapper'>
-                                        <span>Work with any team</span>
-                                        <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
-                                        <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
-                                    </span>
-                                </div>
-                            }>
+                            <Collapsible trigger={collapsible_header('Work with any team')}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
                             </Collapsible>
                             <hr></hr>
-                            <Collapsible trigger={
-                                <div>
-                                    <span className='wrapper'>
-                                        <span>Business analytics</span>
-                                        <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
-                                        <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
-                                    </span>
-                                </div>
-                            }>
+                            <Collapsible trigger={collapsible_header('Business analytics')}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
                             </Collapsible>
                             <hr></hr>
