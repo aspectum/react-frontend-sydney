305d594bb647c7c9452ccb512710f9ac2e92f1f9
Pedro Garcia <drogpe@gmail.com>
2021-01-17 16:32:48 -0300

    AnalyzeTools: Swapped for CollapsibleList component

--- src/components/AnalyzeTools/AnalyzeTools.js
+++ src/components/AnalyzeTools/AnalyzeTools.js
@@ -1,22 +1,26 @@
-import Collapsible from 'react-collapsible'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CollapsibleList from '../CollapsibleList/CollapsibleList'
 import image from './analyzetools.png'
 import './AnalyzeTools.css'
-import { faPlus, faWindowMinimize } from '@fortawesome/free-solid-svg-icons'
 
-// https://www.npmjs.com/package/react-collapsible
 
-function collapsible_header(header_text) {
-    return (
-        <div>
-            <span className='wrapper'>
-                <span>{header_text}</span>
-                <span className='plus'><FontAwesomeIcon icon={faPlus} color='#7575DA' /></span>
-                <span className='minus'><FontAwesomeIcon icon={faWindowMinimize} color='#7575DA' /></span>
-            </span>
-        </div>
-    )
-}
+const items = [
+    {
+        title: 'Organize your data',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+    },
+    {
+        title: 'Always in sync',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+    },
+    {
+        title: 'Work with any team',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+    },
+    {
+        title: 'Business analytics',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+    },
+]
 
 function AnalyzeTools() {
     return (
@@ -29,24 +33,7 @@ function AnalyzeTools() {
                     <div className='margin-wrapper'>
                         <h2>Analyze Your Data With Our Analyze Tools</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                        <div>
-                            <Collapsible trigger={collapsible_header('Organize your data')}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </Collapsible>
-                            <hr></hr>
-                            <Collapsible trigger={collapsible_header('Always in sync')}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </Collapsible>
-                            <hr></hr>
-                            <Collapsible trigger={collapsible_header('Work with any team')}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </Collapsible>
-                            <hr></hr>
-                            <Collapsible trigger={collapsible_header('Business analytics')}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                            </Collapsible>
-                            <hr></hr>
-                        </div>
+                        <CollapsibleList items={items} />
                     </div>
                 </div>
             </div>
