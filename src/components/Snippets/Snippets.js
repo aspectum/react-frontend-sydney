98d6d5abb5138f28e27b0e0111bb0985981fcbb1
Pedro Garcia <drogpe@gmail.com>
2021-01-17 16:13:34 -0300

    Snippets: swapped for CustomList component

--- src/components/Snippets/Snippets.js
+++ src/components/Snippets/Snippets.js
@@ -1,7 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import snippets from './snippets.png';
 import './Snippets.css'
+import CustomList from '../CustomList/CustomList';
+
+const items = [
+    'The text should be more aligned and in a small',
+    'Contrary to popular belief, Lorem Ipsum is not simply',
+    'All the Lorem Ipsum generators on the Internet tend',
+    'Sed ut perspiciatis unde omnis iste natus error sit',
+    'All the Lorem Ipsum generators on the Internet tend',
+]
 
 function Snippets() {
     return (
@@ -13,13 +20,7 @@ function Snippets() {
                 <div className='description'>
                     <h2>Simple, Intuitive And Easy To Use</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                    <ul>
-                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>The text should be more aligned and in a small</span></li>
-                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>Contrary to popular belief, Lorem Ipsum is not simply</span></li>
-                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>All the Lorem Ipsum generators on the Internet tend</span></li>
-                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>Sed ut perspiciatis unde omnis iste natus error sit</span></li>
-                        <li><FontAwesomeIcon icon={faCheck} className='icon-check' /><span>All the Lorem Ipsum generators on the Internet tend</span></li>
-                    </ul>
+                    <CustomList items={items} />
                 </div>
             </div>
         </div>
