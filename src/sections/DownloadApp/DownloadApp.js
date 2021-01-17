e368af6e961010243a28ee34aba5187916f6a6ab
Pedro Garcia <drogpe@gmail.com>
2021-01-17 17:25:16 -0300

    Swapped for respective components

--- src/sections/DownloadApp/DownloadApp.js
+++ src/sections/DownloadApp/DownloadApp.js
@@ -1,5 +1,7 @@
 import { faApple, faGooglePlay } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ButtonAppStore from '../../components/ButtonAppStore/ButtonAppStore'
+import ButtonGooglePlay from '../../components/ButtonGooglePlay/ButtonGooglePlay'
 import './DownloadApp.css'
 
 function DownloadApp() {
@@ -10,24 +12,8 @@ function DownloadApp() {
                     <h2>Start Your 7 Days Free Trial!</h2>
                     <p>SYDNEY is Just What Your Business Needs  adipiscing elit ut elit tellus luctus</p>
                     <div className='stores'>
-                        <div className='button'>
-                            <div className='store_icon'>
-                                <FontAwesomeIcon icon={faApple} color='#FFFFFF' />
-                            </div>
-                            <div>
-                                <p style={{fontSize: '14px', fontWeight: '300'}}>Download on the</p>
-                                <p style={{fontSize: '22px', fontWeight: '600'}}>App Store</p>
-                            </div>
-                        </div>
-                        <div className='button btn2'>
-                            <div className='store_icon'>
-                                <FontAwesomeIcon icon={faGooglePlay} />
-                            </div>
-                            <div>
-                                <p style={{fontSize: '14px', fontWeight: '300'}}>Download on the</p>
-                                <p style={{fontSize: '22px', fontWeight: '600'}}>Google Play</p>
-                            </div>
-                        </div>
+                        <ButtonAppStore />
+                        <ButtonGooglePlay />
                     </div>
                 </div>
             </div>
