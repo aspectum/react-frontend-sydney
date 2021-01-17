92856b6de1ae991e7301316ecaa1238f4bf6e6c9
Pedro Garcia <drogpe@gmail.com>
2021-01-17 17:43:57 -0300

    Used Quotes component

--- src/sections/Testimonials/Testimonials.js
+++ src/sections/Testimonials/Testimonials.js
@@ -1,6 +1,6 @@
 import CompaniesSwiper from '../../components/CompaniesSwiper/CompaniesSwiper';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import Quotes from '../../components/Quotes/Quotes';
+
 import logo_1 from './company_logo_1.png'
 import logo_2 from './company_logo_2.png'
 import logo_3 from './company_logo_3.png'
@@ -22,32 +22,22 @@ function Testimonials() {
                 <h3 style={{fontSize: '26px', fontWeight: '500'}}>More than 2,2 million people across 55,000 companies choose Sydney</h3>
                 <CompaniesSwiper items={companies} />
                 <div className='flex'>
-                    <div className='card'>
-                        <p className='quote-symbol'><FontAwesomeIcon icon={faQuoteLeft} color='#7575DA' /></p>
-                        <p className='quote-text'>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                        <div className='person'>
-                            <div className='portrait'>
-                                <img src={portrait_2} alt='' width='60px' height='60px'></img>
-                            </div>
-                            <div className='who'>
-                                <h3>Amanda Lee</h3>
-                                <p>Ceo & Founder Crix</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <p className='quote-symbol'><FontAwesomeIcon icon={faQuoteLeft} color='#7575DA' /></p>
-                        <p className='quote-text'>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                        <div className='person'>
-                            <div className='portrait'>
-                                <img src={portrait_1} alt='' width='60px' height='60px'></img>
-                            </div>
-                            <div className='who'>
-                                <h3>Adam Cheise</h3>
-                                <p>Head Of Sales, Intel</p>
-                            </div>
-                        </div>
-                    </div>
+                    <Quotes 
+                        quote='Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+                        person={{
+                            name: 'Amanda Lee',
+                            job: 'Ceo & Founder Crix',
+                            portrait: portrait_2
+                        }}
+                    />
+                    <Quotes 
+                        quote='Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.'
+                        person={{
+                            name: 'Adam Cheise',
+                            job: 'Head Of Sales, Intel',
+                            portrait: portrait_1
+                        }}
+                    />
                 </div>
             </div>
         </div>
