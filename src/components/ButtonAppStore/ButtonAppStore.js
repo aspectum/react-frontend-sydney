b918508e86d6fccefcdf0156e640a0ffc1f677f4
Pedro Garcia <drogpe@gmail.com>
2021-01-17 17:24:11 -0300

    Created ButtonAppStore component

---
+++ src/components/ButtonAppStore/ButtonAppStore.js
@@ -0,0 +1,19 @@
+import { faApple } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './ButtonAppStore.css'
+
+function ButtonAppStore() {
+    return (
+        <div className='button-app-store'>
+            <div className='store_icon'>
+                <FontAwesomeIcon icon={faApple} />
+            </div>
+            <div>
+                <p style={{fontSize: '14px', fontWeight: '300'}}>Download on the</p>
+                <p style={{fontSize: '22px', fontWeight: '600'}}>App Store</p>
+            </div>
+        </div>
+    )
+}
+
+export default ButtonAppStore
